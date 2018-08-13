35678c756a44dc80ad371022cee96947860ef5f2
Rúben Gomes <gomesruben21@gmail.com>
2018-08-13 23:34:31 +0100

    Fix eslint issues

--- src/album/index.spec.js
+++ src/album/index.spec.js
@@ -2,7 +2,7 @@ import Album from './index'
 describe('Album', function() {
   context('#find', function() {
     it('should have retrieve albums information', function() {
-      const albums = new Album({
+      new Album({
         token: 'token'
       })
         .find({ ids: '4mLGYDItfdpdfo5p06WgTK' })
